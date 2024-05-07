70e4d9759b12a35a6d7fa48ea62a131885f62333
Harsat <mimkal@mail.ru>
2024-05-07 17:26:42 +0700

    upd to ver 2

--- app/src/main/java/com/example/yandexmaplab/MainActivity.kt
+++ app/src/main/java/com/example/yandexmaplab/MainActivity.kt
@@ -16,12 +16,30 @@ import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
+import android.Manifest
+import android.content.pm.PackageManager
+import android.renderscript.ScriptGroup.Input
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import com.yandex.mapkit.location.Location
+import com.yandex.mapkit.location.LocationListener
+import com.yandex.mapkit.location.LocationStatus
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObject
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.runtime.image.ImageProvider
+import java.security.Permission
 
 var key = "7e992415-c2a9-4e6e-95c6-179221ec4724"
 
 private lateinit var mapView: MapView
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapObjectCollection: MapObjectCollection
+    lateinit var userLocationLayer: UserLocationLayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -51,6 +69,34 @@ class MainActivity : AppCompatActivity() {
             } else {
                 probki.isTrafficVisible = false
             }
+        }
+
+        requestPermitions()
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.mapWindow)
+        userLocationLayer.isVisible = true
+
+        mapObjectCollection = mapView.map.mapObjects.addCollection()
+
+        fun createMark(point: Point ) {
+            mapObjectCollection.addPlacemark(point)
+        }
+
+        val listener = object : InputListener {
+            override fun onMapTap(p0: Map, p1: Point) {
+                createMark(p1)
+            }
+
+            override fun onMapLongTap(p0: Map, p1: Point) {
+                createMark(p1)
+            }
+        }
+
+        mapView.map.addInputListener(listener)
+    }
+
+    private fun requestPermitions(){
+        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 0)
         }
     }
 
